14702834447fb9a977135a797c21415be08d2440
李姿瑩 <sophia@liziyingdeMacBook-Air.local>
2023-11-08 22:32:32 +0800

    New Logo Text

--- johnny/src/components/NavBar/NavBar.jsx
+++ johnny/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
 import './NavBar.css';
 import Logo from '../Logo/Logo';
-
+import Text3D from '../Text3D/Text3D';
 
 function NavBar() {
 
   return (
     <nav className='NavBar'>
-      <Logo />
+      {/* <Logo /> */}
+      <Text3D />
       <div className='Meun'></div>
     </nav>
   );
---
+++ johnny/src/components/Text3D/Text3D.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Text3D.css'
+
+const Text3D = () => {
+  return (
+    <span class="Text3D">Johnny Yeh</span>
+  )
+}
+
+export default Text3D
